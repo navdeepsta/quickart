0530d04806f6a83a554baf5e39cba23dc67305c5
navdeep <navdeepst.1987@gmail.com>
2023-11-11 14:28:06 +1100

    fix: add conditional check for rendering products

--- src/components/ProductSearch/ProductList/index.js
+++ src/components/ProductSearch/ProductList/index.js
@@ -3,9 +3,10 @@ import Product from "../Product";
 import './ProductList.css';
 
 export default function ProductList(props) {
+  const products = props.products;
   return (
     <div className="product-list">
-      {props.products.map((product) => (
+      {products && products.map((product) => (
         <Product key={product.uniq_id} product={product} />
       ))}
     </div>
