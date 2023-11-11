c24974af5e19128c47e0dcff58fba84e1c813e09
navdeep <navdeepst.1987@gmail.com>
2023-11-11 15:28:28 +1100

    chore: modify ProductSearch component to add ProductFilter

--- src/components/ProductSearch/index.js
+++ src/components/ProductSearch/index.js
@@ -1,11 +1,37 @@
-import React from 'react';
-import ProductList from './ProductList';
-import products from './productData.json';
-
+import React, { useEffect, useState } from "react";
+import ProductList from "./ProductList";
+import productData from "./productData.json";
+import ProductFilter from "./ProductFilter";
 export default function ProductSearch() {
-  return (
-    <div>
+  const [originalProducts, setOriginalProducts] = useState(null);
+  const [products, setProducts] = useState(null);
+
+  useEffect(() => {
+    setOriginalProducts(productData);
+  }, []);
+
+  useEffect(() => {
+    setProducts(originalProducts);
+  }, [originalProducts]);
+
+  const filterProducts = (filteredProducts) => {
+    setProducts(filteredProducts);
+  };
+
+  if (originalProducts) {
+    return (
+      <div>
+        <ProductFilter
+          originalProducts={originalProducts}
+          products={products}
+          onProductFilter={(filteredProducts) =>
+            filterProducts(filteredProducts)
+          }
+        />
         <ProductList products={products} />
-    </div>
-  )
+      </div>
+    );
+  } else {
+    return <h1>Loading</h1>;
+  }
 }
