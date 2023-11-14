2eec438b2f2bdb052aa7e5b599c591f8c9b5a5da
navdeep <navdeepst.1987@gmail.com>
2023-11-14 20:02:47 +1100

    refactor: clear the search input field on onBlur event

--- src/components/ProductSearch/ProductFilter/SearchFilter/index.js
+++ src/components/ProductSearch/ProductFilter/SearchFilter/index.js
@@ -2,17 +2,32 @@ import React, { useEffect, useState } from "react";
 import "./SearchFilter.css";
 export default function SearchFilter({ products, onSearchFilter }) {
   const [searchItem, setSearchItem] = useState("");
+  const [focus, setFocus] = useState(false);
 
   useEffect(() => {
-    onSearchFilter(products.filter(product => {
-       return product.title.toLowerCase().includes(searchItem)
-    }))
+    if (focus) {
+      onSearchFilter(
+        products.filter((product) => {
+          return product.title.toLowerCase().includes(searchItem);
+        })
+      );
+    }
   }, [searchItem]);
+
+  const handleBlur = () => {
+    setFocus(false);
+    setSearchItem("");
+  };
 
   return (
     <div className="search">
-      <input type="text" placeholder="Search products" 
-      onChange={(e)=>setSearchItem(e.target.value)} 
+      <input
+        type="text"
+        placeholder="Search products"
+        onChange={(e) => setSearchItem(e.target.value)}
+        onFocus={() => setFocus(true)}
+        onBlur={handleBlur}
+        value={searchItem}
       />
     </div>
   );
