aff99c73c017950c7b0e7d2c2da3745e079cc068
navdeep <navdeepst.1987@gmail.com>
2023-11-15 12:57:11 +1100

    refactor: implement routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import ProductSearch from './components/ProductSearch';
-import './App.css';
+import Main from "./components/Main";
+import ProductDetails from "./components/ProductDetails";
+import "./App.css";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<Main />}></Route>
+      <Route path="/products" element={<Main />}></Route>
+      <Route path="*" element={<h1>404</h1>}></Route>
+    </>
+  )
+);
 
 function App() {
-  return (
-   <ProductSearch />
-  )
+  return <RouterProvider router={router} />;
 }
 
 export default App;
