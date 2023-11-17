3b46789a196d20bf003770a09d5088e020eeeca0
navdeep <navdeepst.1987@gmail.com>
2023-11-17 11:29:32 +1100

    feat: implement pagination

---
+++ src/components/ProductSearch/ProductList/Pagination.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+export default function Pagination({
+  currentPage,
+  products,
+  pageSize,
+  onPageChange,
+}) {
+  const totalPages = Math.ceil(products.length / pageSize);
+
+  const handlePageChange = (newPage) => {
+    if (newPage >= 1 && newPage <= totalPages) {
+      onPageChange(newPage);
+    }
+  };
+  const generatePageNumbers = () => {
+    const maxButtonsToShow = 5;
+    const delta = Math.floor(maxButtonsToShow / 2);
+
+    let start = Math.max(1, currentPage - delta);
+    let end = Math.min(totalPages, start + maxButtonsToShow - 1);
+
+    if (end - start < maxButtonsToShow - 1) {
+      start = Math.max(1, end - maxButtonsToShow + 1);
+    }
+
+    return Array.from({ length: end - start + 1 }, (_, index) => start + index);
+  };
+  // Generate an array of page numbers
+  const pageNumbers = generatePageNumbers();
+
+  return (
+    <div style={{ textAlign: "center" }}>
+      {/* Pagination buttons */}
+      <button
+        onClick={() => handlePageChange(currentPage - 1)}
+        disabled={currentPage === 1}
+      >
+        Previous
+      </button>
+      {pageNumbers.map((page) => (
+        <button
+          key={page}
+          onClick={() => handlePageChange(page)}
+          style={{ fontWeight: currentPage === page ? "bold" : "normal" }}
+        >
+          {page}
+        </button>
+      ))}
+      <button
+        onClick={() => handlePageChange(currentPage + 1)}
+        disabled={currentPage === totalPages}
+      >
+        Next
+      </button>
+    </div>
+  );
+}
--- src/components/ProductSearch/ProductList/index.js
+++ src/components/ProductSearch/ProductList/index.js
@@ -1,19 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Product from "../Product";
+import Pagination from "./Pagination";
 import "./ProductList.css";
 
-export default function ProductList({products, onProductClick}) {
- 
+export default function ProductList({ products, onProductClick }) {
+  const pageSize = 20; // Set your desired page size
+  const [prods, setProds] = useState([]); 
+  const [currentPage, setCurrentPage] = useState(1);
+
+  useEffect(() => {
+    setProds(products);
+    setCurrentPage(1);
+  }, [products]);
+
   return (
-    <div className="product-list">
-      {products &&
-        products.map((product) => (
-          <Product
-            key={product.uniq_id}
-            product={product}
-            onProductClick={()=>onProductClick(product)}
-          />
-        ))}
-    </div>
+    <>
+      <Pagination
+        currentPage={currentPage}
+        onPageChange={(newPage) => setCurrentPage(newPage)}
+        products={prods}
+        pageSize={pageSize}
+      />
+      <div className="product-list">
+        {prods
+          .slice((currentPage - 1) * pageSize, currentPage * pageSize)
+          .map((product) => (
+            <Product
+              key={product.uniq_id}
+              product={product}
+              onProductClick={() => onProductClick(product)}
+            />
+          ))}
+      </div>
+      <Pagination
+        currentPage={currentPage}
+        onPageChange={(newPage) => setCurrentPage(newPage)}
+        products={prods}
+        pageSize={pageSize}
+      />
+    </>
   );
 }
