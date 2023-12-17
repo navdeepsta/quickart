63e16081b360cae3ae08528d51b49954fb12eb5b
navdeep <navdeepst.1987@gmail.com>
2023-12-18 10:04:24 +1100

    Refactor: add image change logic and conditionally render InStock

--- src/components/ProductDetails/index.js
+++ src/components/ProductDetails/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
@@ -7,6 +7,13 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./PageDetails.css";
 export default function ProductDetails({ product, displayProduct }) {
+  const images = product?.images?.split("|");
+  const [selectedImage, setSelectedImage] = useState(images[0]);
+
+  const handleImageSelect = (image) => {
+    setSelectedImage(image);
+  };
+
   const settings = {
     accessibility: true,
     dots: true,
@@ -14,9 +21,34 @@ export default function ProductDetails({ product, displayProduct }) {
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 300,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
   if (product) {
-    const images = product.images.split("|");
     return (
       <>
         <Button variant="outlined" onClick={() => displayProduct(false)}>
@@ -26,13 +58,16 @@ export default function ProductDetails({ product, displayProduct }) {
           <Grid container spacing={2}>
             <Grid item xs={12} sm={12} md={6}>
               <div className="image-container">
-                <img className="main-image" src={images[0] && images[0]} />
+                <img className="main-image" src={selectedImage} />
 
                 <div className="image-slider">
                   <Slider {...settings}>
                     {images.map((image) => (
-                      <div className="image-slider-images">
-                        <img key={image} src={image} />
+                      <div className="image-slider-images" key={image}>
+                        <img
+                          src={image}
+                          onClick={() => handleImageSelect(image)}
+                        />
                       </div>
                     ))}
                   </Slider>
@@ -44,9 +79,20 @@ export default function ProductDetails({ product, displayProduct }) {
                 <>
                   <p>{product.title}</p>
                   <p>Brand: {product.brand}</p>
-                  <p>${product.price}</p>
-                  <p>{product.availability}</p>
-                  <Button variant="outlined">Add To Cart</Button>
+                  <p className="product-price">${product.price}</p>
+                  <span
+                    className={
+                      product.availability === "InStock"
+                        ? "in-stock"
+                        : "out-of-stock"
+                    }
+                  >
+                    {product.availability ? product.availability : "OutOfStock"}
+                  </span>
+                  <br />
+                  <Box mt={2}>
+                  <Button variant="outlined" >Add To Cart</Button>
+                  </Box>
                 </>
               </Box>
             </Grid>
