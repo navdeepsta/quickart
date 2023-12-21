74ca9eb07647d3d28480bff8a3b60708b60339cd
navdeep <navdeepst.1987@gmail.com>
2023-12-21 17:58:40 +1100

    refactor: add style to Navigation

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,22 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import "./Navigation.css";
 
 export default function Navigation({ shoppingCart, state }) {
   return (
-    <>
+    <nav className="navigation">
       {!state?.loggedIn ? (
-        <Link to="/signin">Login</Link>
+        <Link to="/signin">
+          <AccountCircle fontSize="medium" color="primary" />
+        </Link>
       ) : (
         <>
-          {state?.user.firstName ? state?.user.firstName : state?.user.username}
+          <strong>
+            {state?.user.firstName
+              ? state?.user.firstName
+              : state?.user.username}
+          </strong>
           <Link to="/signin" state={state}>
-            Profile
+            <AccountCircle fontSize="medium" color="primary" />
           </Link>
         </>
       )}
-      <Link to="/cart" state={{ cart : shoppingCart }}>
-        Cart : {shoppingCart?.reduce((acc,curr)=> acc + curr.quantity, 0)} items  
-      </Link>
-    </>
+      <div className="cart">
+        <Link to="/cart" state={{ cart: shoppingCart }} underline="none">
+          <AddShoppingCartIcon fontSize="medium" color="primary" />
+        </Link>{" "}
+        {shoppingCart?.reduce((acc, curr) => acc + curr.quantity, 0)}
+      </div>
+    </nav>
   );
 }
